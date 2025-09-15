2d81626c5028444a5214bfcf46311e78d3b176bf
ParfenovPK <ppkonstantinovch@gmail.com>
2025-09-15 17:42:54 +0300

    fixed Error line too long in Task_1_lesson_4

--- homework/pavel_parfenov/homework_4/Task_1_lesson_4.py
+++ homework/pavel_parfenov/homework_4/Task_1_lesson_4.py
@@ -1,5 +1,10 @@
-my_dict = {"my_tuple": (1, 2, 3, 4, 5), "my_list": ["a", "b", "c", "d", "f"],
-           "dict_test": {"name": "Alice", "age": 30, "mail": "git_googl@gmail.com", "city": "London", "phone_number": 89997775544}, "my_set": {2, 3, "d", "f", False}}
+my_dict = {"my_tuple": (1, 2, 3, 4, 5),
+           "my_list": ["a", "b", "c", "d", "f"],
+           "dict_test": {"name": "Alice", "age": 30,
+                         "mail": "git_googl@gmail.com",
+                         "city": "London",
+                         "phone_number": 89997775544},
+           "my_set": {2, 3, "d", "f", False}}
 
 print(my_dict["my_tuple"][-1])
 
