a1c4302463db03497c78b29dff5595cc80a04d63
ParfenovPK <ppkonstantinovch@gmail.com>
2025-09-15 13:14:19 +0300

    fixed Error in Task_4_lesson_3.py

--- homework/pavel_parfenov/Homework_3/Task_4_lesson_3.py
+++ homework/pavel_parfenov/Homework_3/Task_4_lesson_3.py
@@ -4,7 +4,7 @@
 right_triangle_leg_1 = int(input("Введите значение катета_1: "))
 right_triangle_leg_2 = int(input("Введите значение катета_2: "))
 hypotenuse_right_triangle = (right_triangle_leg_1**2 + right_triangle_leg_2**2) ** 0.5
-area_right_triangle = (right_triangle_leg_1 * right_triangle_leg_2)/2
+area_right_triangle = (right_triangle_leg_1 * right_triangle_leg_2) / 2
 
 print(f"Гипотенуза прямоугольного треугольника: {hypotenuse_right_triangle}")
 print(f"Площадь прямоугольного треугольника: {area_right_triangle}")
