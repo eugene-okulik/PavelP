d246ed4e1ebb08388742924664eee08a2462374d
ParfenovPK <ppkonstantinovch@gmail.com>
2025-09-16 19:02:40 +0300

    Task_1_lesson_5 dane, Task_2_lesson_5 in progress, Task_3_lesson_5 dane

---
+++ homework/pavel_parfenov/Homework_5/Task_3_lesson_5.py
@@ -0,0 +1,18 @@
+"""Задание 3
+
+Даны такие списки:
+
+students = ['Ivanov', 'Petrov', 'Sidorov']
+
+subjects = ['math', 'biology', 'geography']
+
+Распечатайте текст, который будет использовать данные из этих списков. Текст в итоге должен выглядеть так:
+
+Students Ivanov, Petrov, Sidorov study these subjects: math, biology, geography"""
+
+students = ['Ivanov', 'Petrov', 'Sidorov']
+
+subjects = ['math', 'biology', 'geography']
+
+print(
+    f"Student {students[0]}, {students[1]}, {students[2]} study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}")
