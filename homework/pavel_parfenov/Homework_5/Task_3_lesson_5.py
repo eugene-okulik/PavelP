fa3f22c99ba01a14dedd702e31a6984e578bf2d3
ParfenovPK <ppkonstantinovch@gmail.com>
2025-09-17 17:58:29 +0300

    Task_2_lesson_5 done, Task_3_lesson_5 done

--- homework/pavel_parfenov/Homework_5/Task_3_lesson_5.py
+++ homework/pavel_parfenov/Homework_5/Task_3_lesson_5.py
@@ -6,13 +6,15 @@ students = ['Ivanov', 'Petrov', 'Sidorov']
 
 subjects = ['math', 'biology', 'geography']
 
-Распечатайте текст, который будет использовать данные из этих списков. Текст в итоге должен выглядеть так:
+Распечатайте текст, который будет использовать данные из этих списков.
+ Текст в итоге должен выглядеть так:
 
-Students Ivanov, Petrov, Sidorov study these subjects: math, biology, geography"""
+Students Ivanov, Petrov, Sidorov study these subjects:
+ math, biology, geography"""
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
 
 subjects = ['math', 'biology', 'geography']
 
-print(
-    f"Student {students[0]}, {students[1]}, {students[2]} study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}")
+print(f"Student", ", ".join(students),
+      "study these subjects:", ", ".join(subjects))
