aedad8b3ed292f62ffa0014309fe6a9e72f8813d
ParfenovPK <ppkonstantinovch@gmail.com>
2025-10-15 16:41:58 +0300

    Task_1_lesson_6 fixed code standards

--- homework/pavel_parfenov/Homework_6/Task_1_lesson_6.py
+++ homework/pavel_parfenov/Homework_6/Task_1_lesson_6.py
@@ -5,14 +5,17 @@
 # идет запятая или точка, этот знак препинания должен идти после того же слова,
 #  но уже преобразованного.
 
-my_text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+my_text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, " \
+    "facilisis vitae semper at, dignissim vitae libero"
 my_list = my_text.split()
 
 fin_text = []
 for word in my_list:
-    if word[-1] in [",", "."]:
+    if word[-1] in [",", "."]:  # поиск слов у которых на конце знаки припинания
+        # Добавляем 'ing' к словам, пропуская знаки препинания.
         new_word = fin_text.append(word[:-1] + "ing" + word[-1])
     else:
+        # Добавляем 'ing' к словам, у которых нет знаков препинания.
         fin_text.append(word + "ing")
 
 print(' '.join(fin_text))
